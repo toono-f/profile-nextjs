cd77b85ee11a222ad4ccfca06c82e2b79d0b1a7f
toono <52028799+toono-f@users.noreply.github.com>
2022-09-24 21:32:15 +0900

    不要記述削除

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,21 +1,26 @@
 import styles from "@/components/organisms/Header/Header.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
 // import { useRouter } from "next/router";
 // import { useState } from "react";
 
 export const Header = () => {
+  const router = useRouter();
   // const [isOpen, setIsOpen] = useState<boolean>(false);
-  // const router = useRouter();
-
   // useEffect(() => setIsOpen(false), [router]);
 
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>
-        <Link href="/">
-          <a>Otono&apos;s portfolio</a>
-        </Link>
+        {router.pathname === "/" ? (
+          <span>Otono&apos;s portfoli</span>
+        ) : (
+          <Link href="/">
+            <a>Otono&apos;s portfolio</a>
+          </Link>
+        )}
       </h1>
+
       {/* <nav className={isOpen ? styles.navi_active : styles.navi}>
         <ul className={styles.list}>
           <li>
@@ -25,9 +30,7 @@ export const Header = () => {
           </li>
         </ul>
       </nav> */}
-
       {/* <div className={isOpen ? styles.cover_active : styles.cover} onClick={() => setIsOpen(false)} /> */}
-
       {/* <button className={isOpen ? styles.button_active : styles.button} onClick={() => setIsOpen((prev) => !prev)}>
         <span className={styles.buttonline}>
           {[...Array(3)].map((_, i) => (
