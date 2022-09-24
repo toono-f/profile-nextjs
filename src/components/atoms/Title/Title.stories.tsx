59944dc4f5a4c1cfaa336f2b65474c9521e0f6d6
toono <52028799+toono-f@users.noreply.github.com>
2022-09-24 21:59:17 +0900

    story修正

--- src/components/atoms/Title/Title.stories.tsx
+++ src/components/atoms/Title/Title.stories.tsx
@@ -4,5 +4,7 @@ import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 export default { component: Title } as ComponentMeta<typeof Title>;
 
 export const Index: ComponentStoryObj<typeof Title> = {
-  args: {},
+  args: {
+    children: <span>タイトル</span>,
+  },
 };
