07216f97356efebc197afbd8a9b065329b0d4fe6
toono <52028799+toono-f@users.noreply.github.com>
2022-10-04 19:42:38 +0900

    リファクタ

--- src/components/organisms/MovieModal/MovieModal.stories.tsx
+++ src/components/organisms/MovieModal/MovieModal.stories.tsx
@@ -4,6 +4,8 @@ import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 type T = typeof MovieModal;
 type Story = ComponentStoryObj<T>;
 
-export default { component: MovieModal, args: {} } as ComponentMeta<T>;
+export default { component: MovieModal } as ComponentMeta<T>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {},
+};
