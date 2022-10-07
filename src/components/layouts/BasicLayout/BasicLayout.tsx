6be8469fcb56b929e4129b8e76b5a6cf74c81cc1
toono <52028799+toono-f@users.noreply.github.com>
2022-10-07 23:38:21 +0900

    追加

--- src/components/layouts/BasicLayout/BasicLayout.tsx
+++ src/components/layouts/BasicLayout/BasicLayout.tsx
@@ -3,12 +3,23 @@ import { Footer } from "@/components/organisms/Footer";
 import { Header } from "@/components/organisms/Header";
 import { Seo } from "@/data/Seo";
 import { NextSeo } from "next-seo";
+import Head from "next/head";
 import { ReactElement } from "react";
 
 export const BasicLayout = (page: ReactElement) => {
   return (
     <>
       <NextSeo {...Seo} />
+      <Head>
+        <meta name="keywords" content="" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var viewport=document.querySelector('meta[name="viewport"]');function switchViewport(){var value=window.outerWidth>375?"width=device-width,initial-scale=1.0":"width=375";if(viewport.getAttribute("content")!==value){viewport.setAttribute("content",value)}} switchViewport();window.addEventListener("resize",switchViewport);`,
+          }}
+        />
+      </Head>
       <div className={styles.layout}>
         <Header />
         {page}
