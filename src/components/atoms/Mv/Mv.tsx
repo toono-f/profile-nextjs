2d36110bc64c9f27545f9ccb77983d605cbffab1
toono <52028799+toono-f@users.noreply.github.com>
2022-10-16 20:36:46 +0900

    アイコン差し替え

--- src/components/atoms/Mv/Mv.tsx
+++ src/components/atoms/Mv/Mv.tsx
@@ -4,7 +4,7 @@ export const Mv = () => {
   return (
     <div className={styles.mv}>
       <p className={styles.icon}>
-        <img src="/images/index/mv.jpg" alt="" width="300" height="300" decoding="async" />
+        <img src="/images/index/mv.png" alt="" width="300" height="300" decoding="async" />
       </p>
       <p className={styles.text}>Otono</p>
       <p>Front End Engineer</p>
