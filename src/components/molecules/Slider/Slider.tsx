53208631c983f79122bc1b69807f83c3164cef7c
toono <52028799+toono-f@users.noreply.github.com>
2022-10-22 16:32:29 +0900

    slider実装

---
+++ src/components/molecules/Slider/Slider.tsx
@@ -0,0 +1,36 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
+export const Slider = () => {
+  return (
+    <>
+      <Splide
+        aria-label="私のお気に入りの画像集"
+        options={{
+          autoplay: true,
+          interval: 3000,
+        }}
+      >
+        <SplideSlide>
+          <img className="slide-img" src="https://www.pakutaso.com/shared/img/thumb/shikun20220402_114719-2_TP_V.jpg" alt="かわいい猫の画像 part1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img className="slide-img" src="https://www.pakutaso.com/shared/img/thumb/shikun20220402_122123_TP_V.jpg" alt="かわいい猫の画像 part2" />
+        </SplideSlide>
+        <SplideSlide>
+          <img className="slide-img" src="https://www.pakutaso.com/shared/img/thumb/sikun_20220402-180657-2_TP_V.jpg" alt="かわいい猫の画像 part3" />
+        </SplideSlide>
+      </Splide>
+
+      {/* 画像の高さを揃えて表示させるために以下スタイルを適用 */}
+      <style jsx>{`
+        .slide-img {
+          display: block;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      `}</style>
+    </>
+  );
+};
