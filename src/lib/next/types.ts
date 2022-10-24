8d2f30c9f1b434c5487a0bfbd9c1f7333db6771c
toono <52028799+toono-f@users.noreply.github.com>
2022-10-24 15:30:46 +0900

    型修正

--- src/lib/next/types.ts
+++ src/lib/next/types.ts
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import type { ReactElement, ReactNode } from "react";
+import type { ReactElement } from "react";
 
-export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
-  getLayout: (page: ReactElement) => ReactNode;
+export type NextPageWithLayout<P = Record<never, never>, IP = P> = NextPage<P, IP> & {
+  getLayout: (page: ReactElement) => ReactElement;
 };
 
 export type AppPropsWithLayout = AppProps & {
