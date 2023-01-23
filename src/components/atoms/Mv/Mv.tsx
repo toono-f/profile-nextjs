feb6fe17fd26e1ea793ca3388970c871087e22da
toono <52028799+toono-f@users.noreply.github.com>
2023-01-23 16:10:03 +0900

    webp追加

--- src/components/atoms/Mv/Mv.tsx
+++ src/components/atoms/Mv/Mv.tsx
@@ -4,7 +4,10 @@ export const Mv = () => {
   return (
     <div className={styles.mv}>
       <p className={styles.icon}>
-        <img src="/images/index/mv.png" alt="" width="300" height="300" decoding="async" />
+        <picture>
+          <source srcSet="/images/index/mv.webp" type="image/webp" />
+          <img src="/images/index/mv.png" alt="" width="300" height="300" decoding="async" />
+        </picture>
       </p>
       <p className={styles.text}>Otono</p>
       <p>Front End Engineer</p>
